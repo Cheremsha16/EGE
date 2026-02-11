440dc91b4a0399a99860e465c31421c32ccb3470
Mustvaaa1615 <annamusatova8716@mail.ru>
2026-02-11 19:56:37 +0200

    https://kompege.ru/variant?kim=25154552

--- Task-5/theory.py
+++ Task-5/theory.py
@@ -10,6 +10,9 @@ print(f"{num:0}")
 print(hex(num)[2:])
 print(f"{num:x}")
 
+# Перевод в 10 систему
+# int(num, sys), где sys - система ИЗ КОТОРОЙ ПЕРЕВОДИМ
+
 # Перевод любую систему счисления (2 <= sys <= 9)
 def convert(num, sys):
     res =''
@@ -39,3 +42,32 @@ print(num_bin.count('1'))
 print(sum(map(int, num_bin)))
 # Сумма цифр любой системы (2 <= sys <= 36)
 print(sum(map(lambda x: int (x, 36), num_bin)))
+
+# Замена символов в строке
+# Заменить N символов слева - R[N:]
+# Заменить N символов справа - R[:-N]
+# Заменить ВСЕ символы в строке (все 0 поменять на 1)
+R = '1010'
+R = R.replace('0', '1')
+
+# Заменить ВСЕ символы в строке (все 1 поменять на 3, 3 поменять на 1)
+R = R.replace('1', '*')
+R = R.replace('3', '1')
+R = R.replace('*', '3')
+
+# Усложнённые варианты вопросов
+# Максимальное N при максимальном R
+ans.append([R, N])
+print(max(ans))
+# Минимальное N при минимальном R
+ans.append([R, N])
+print(min(ans))
+# Минимальное N при максимальном R
+ans.apanspend([R, N])
+ans = sorted(ans, key=lambda x:(-x[0], x[1]))
+print(ans[0])
+# Максимальное N при минимальном R
+ans.apanspend([R, N])
+ans = sorted(ans, key=lambda x:(x[0], -x[1]))
+print(ans[0])
+
---
+++ Task-сам.раб.py
@@ -0,0 +1,32 @@
+from turtle import *
+
+screensize(5000,5000)
+tracer(False)
+m=10
+
+for i in range(2):
+    fd(14*m)
+    lt(270)
+    bk(12*m)
+    rt(90)
+up()
+fd(9*m)
+rt(90)
+bk(7*m)
+lt(90)
+down()
+for i in range(2):
+    fd(13*m)
+    rt(90)
+    fd(6*m)
+    rt(90)
+up()
+
+for x in range(9, 15):
+    for y in range(1, 8):
+        goto(x * m, y * m)
+        dot(3, "red")
+
+update()
+done()
+print(((15*13)+(14*7))-42)
