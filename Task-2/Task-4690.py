c688c33762cb748dbf1429eb653a9db3c1c20084
Mustvaaa1615 <annamusatova8716@mail.ru>
2026-03-18 19:28:41 +0200

    2 задания

--- Task-2/Task-4690.py
+++ Task-2/Task-4690.py
@@ -3,6 +3,6 @@ for x in 0,1:
     for y in 0,1:
         for z in 0,1:
             for w in 0,1:
-                f = not (w <= (x == y)) and (z <= x)
-                if f:
+                f= not(y <= x) or (z <= w) or (not z)
+                if not f:
                     print(w, x, y, z)
