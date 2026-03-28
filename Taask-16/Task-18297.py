0aa22add04f98b5ed28591afdb0d374c30cc897f
Mustvaaa1615 <annamusatova8716@mail.ru>
2026-03-28 19:49:27 +0200

    23 задания

--- Taask-16/Task-18297.py
+++ Taask-16/Task-18297.py
@@ -2,7 +2,7 @@ from functools import lru_cache
 
 @lru_cache(None)
 def F(n):
-    if n > 10: return n-1
+    if n < 10: return n-1
     if n >= 10 and n % 2 == 0: return 3  * n - 1 + F(n-3)
     if n >= 10 and n % 2 != 0: return 5 * n + 2 + F(n-4)
 
--- Task-4606.py
+++
@@ -1,9 +0,0 @@
-from itertools import permutations
-
-graph = 'AC CE EG GF FA AB BF CD DH HE'.split()
-matrix = '68 47 45 237 368 15 248 157'.split()
-
-print(*range(1,9))
-for i in permutations('ABCDEFGH'):
-    if all(str(i.index(x)+1) in matrix[i.index(y)] for x, y in graph):
-        print(*i)
