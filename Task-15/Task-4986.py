bb6c05a949c53b9db9b57b75ba651567f99c6066
Mustvaaa1615 <annamusatova8716@mail.ru>
2026-04-25 19:49:41 +0200

    задания 15

---
+++ Task-15/Task-4986.py
@@ -0,0 +1,13 @@
+def DEL(n, m):
+    return n % m == 0
+
+def f(x):
+    B = 50 <= x <= 70
+    return B <= DEL(x, A) or (DEL(x, 23) <= (not B))
+
+ans =[]
+for A in range(1, 1000):
+    if all(f(x)for x in range(1,1000)):
+        ans.append(A)
+print(max(ans)) #69
+
--- Task-15/Task-4989.py
+++ Task-15/Task-4989.py
@@ -1,7 +1,16 @@
 def DEL(n, m):
-    return (xB) => (DEL(x, 17) => (xA))
+    return n % m == 0
 
-for A in range(1, 1000):
-    if all(DEL(n, m) for n in range(1, 1000) for m in range(1, 1000)):
-        print(A)
-        break
+
+def f(x):
+    B = 20 <= x <= 80
+    A = A1 <= x <= A2
+    return B <= (DEL(x, 17) <= A)
+
+
+ans = []
+for A1 in range(1, 1000):
+    for A2 in range(A1 + 1, 1000):
+        if all(f(x) for x in range(1, 1000)):
+            ans.append(A2 - A1)
+print(min(ans))
